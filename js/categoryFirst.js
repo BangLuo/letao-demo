1307830206d2c026b656bea048eeeee0c1d94797
LuoDan <1178886976@qq.com>
2018-06-20 15:35:42 +0800

    一级分类完成

--- js/categoryFirst.js
+++ js/categoryFirst.js
@@ -1,24 +1,92 @@
+var  page = 1;
+var pageSize = 5;
+//总页数
+var totalPage = null;
 $(function () {
 
-    //
-    $.ajax({
-     type:'get',
-     url: `${APP.baseUrl}/category/queryTopCategoryPaging`,
-     data: {
-         page: 1,
-         pageSize: 10
-     },
-     success:function (response) {
-             console.log(response);
+    //查询数据 渲染数据
+     getData();
+     $('#prev').on('click', function () {
+        page--;
+        if(page < 1){
+            page = 1;
+            alert('已经是第一页了');
+        }
+        getData();
+     })
 
-             var html = template('categoryFirstTpl', response );
-             console.log(html);
-             $('#categoryFirstTplBox').html(html);
-     }
+     $('#next').on('click', function () {
+        page++;
 
-    })
+        if(page > totalPage){
+            page = totalPage;
+            alert('没有更多的数据了')
+        }
+         getData()
+     })
+
+     $('#addCategoryConfirm').on('click' , function () {
+
+        var categoryName = $.trim($('#addCategoryInput').val() );
+
+        if(categoryName){
+            //console.log(categoryName);
+            addCategoryFn(categoryName);
+        }else{
+            alert('您还没有输入内容');
+            return;
+        }
+     })
+
 
 
 
 
 });
+//查询数据  计算总条数 进行展示
+function getData() {
+    $.ajax({
+     type:'get',
+     url: `${APP.baseUrl}/category/queryTopCategoryPaging`,
+     data: {
+         page: page,
+         pageSize: pageSize
+     },
+     success:function (response) {
+            // console.log(response);
+             if(response.error){
+                location.href = "login.html"
+             }else{
+
+             var html = template('categoryFirstTpl', response );
+            // console.log(html);
+             $('#categoryFirstTplBox').html(html);
+
+            }
+            //计算总页数  ---response.total 数据总条数
+            totalPage = Math.ceil(response.total/pageSize);
+
+
+     }
+
+    })
+
+}
+
+//添加一级分类
+function addCategoryFn(categoryName) {
+    $.ajax({
+        type:'post',
+        url: `${APP.baseUrl}/category/addTopCategory`,
+        data:{
+            categoryName: categoryName
+        },
+        success: function(response) {
+            if(response.success){
+                location.reload();
+            }else{
+                alert('您没有添加成功');
+            }
+        }
+    })
+}
--- js/categorySecond.js
+++ js/categorySecond.js
@@ -1,18 +1,53 @@
+var  page = 1;
+var pageSize = 3;
+//总页数
+var totalPage = null;
+
 $(function () {
+
+
+    //查询数据 渲染数据
+     getData();
+     $('#prev').on('click', function () {
+        page--;
+        if(page < 1){
+            page = 1;
+            alert('已经是第一页了');
+        }
+        getData();
+     })
+
+     $('#next').on('click', function () {
+        page++;
+
+        if(page > totalPage){
+            page = totalPage;
+            alert('没有更多的数据了')
+        }
+         getData()
+     })
+
+
+
+})
+
+function getData() {
+
     $.ajax({
      type:'get',
      url: `${APP.baseUrl}/category/querySecondCategoryPaging`,
      data: {
-         page: 1,
-         pageSize: 10
+         page: page,
+         pageSize: pageSize
      },
      success:function (response) {
-             console.log(response);
-
+             //console.log(response);
              var html = template('categorySecondTpl', response );
-             console.log(html);
+             //console.log(html);
+             //计算总页数  ---response.total 数据总条数
+             totalPage = Math.ceil(response.total/pageSize);
              $('#categorySecondTplBox').html(html);
      }
 
     })
-})
+}
